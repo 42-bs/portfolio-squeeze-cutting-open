b7e0a0fa1d365cbf7b13854a5d6163a57b08927d
Lucas Pires Nogueira <piresnogueiralucas@gmail.com>
2023-08-03 15:51:32 -0300

    crop in array img

--- app/crop_img.py
+++ app/crop_img.py
@@ -1,11 +1,21 @@
 from PIL import Image
+from typing import List, Tuple
 
 
 class CropImg:
-    def __init__(self, img: Image.Image, pos_to_crop: tuple) -> None:
+    def __init__(
+        self,
+        img: Image.Image or List[Image.Image],
+        box: Tuple[int, int, int, int],
+    ) -> None:
         self.img = img
-        self.pos_to_crop = pos_to_crop
+        self.box = box
+        self.new_img: Image.Image or List[Image.Image] = None
 
-    def crop(self) -> Image.Image:
-        cropped_image = self.img.crop(self.pos_to_crop)
-        return cropped_image
+    def crop(self) -> Image.Image or List[Image.Image]:
+        if isinstance(self.img, list):
+            self.new_img = [img.crop(self.box) for img in self.img]
+            return self.new_img
+        else:
+            self.new_img = self.img.crop(self.box)
+            return self.new_img
--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 import os
+import sys
+
 from crop_img import CropImg
 from pdf_img import PdfToImg
 
 
 if __name__ == "__main__":
-    file_path = "tech_draw.pdf"
+    file_path = "pdf/tech_draw.pdf"
 
     if not file_path:
         print("No file path entered. Exiting...")
     else:
         iPdfToImg = PdfToImg(file_path)
         images = iPdfToImg.pdf_to_img()
-
-        iCropImgs = CropImg(images[0], (100, 400, 400, 900))
-        cropped_image = iCropImgs.crop()
-
-        cropped_image.save(os.path.join(os.getcwd(), "cropped_image.png"))
+        
+        iCropImgs = CropImg(images, (100, 0, 1000, 2000))
+        iCropImgs.crop()
+        for img in iCropImgs.new_img:
+            img.show()
